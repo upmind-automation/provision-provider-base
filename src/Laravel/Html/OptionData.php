4ee6c872e444afabac7c7b977a8d61f7405e7bad
Harry Lewis <harry@upmind.com>
2022-10-11 11:09:49 +0100

    Add optional group_label to HtmlField OptionData

--- src/Laravel/Html/OptionData.php
+++ src/Laravel/Html/OptionData.php
@@ -10,6 +10,7 @@ use Upmind\ProvisionBase\Provider\DataSet\Rules;
 /**
  * HTML field select/radio option data set.
  *
+ * @property-read string|null $group_label Optional option group label
  * @property-read string $label Human-readable option label
  * @property-read mixed $value Option value
  */
@@ -18,6 +19,7 @@ class OptionData extends DataSet
     public static function rules(): Rules
     {
         return new Rules([
+            'group_label' => ['nullable', 'string'],
             'label' => ['required', 'string'],
             'value' => ['present'],
         ]);
