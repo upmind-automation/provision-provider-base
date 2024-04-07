9d01c64d598529e560db9961187e68f821c99d63
Roussetos Karafyllakis <35-roussks@users.noreply.git.upmind.io>
2024-04-07 19:46:26 +0100

    Revert "removed guzzle stack history middleware and relevant properties/methods. Updated CHANGELOG"
    This reverts commit 647d7752501da5ebf594df20d4f6ad48184be06a.

--- src/Provider/BaseCategory.php
+++ src/Provider/BaseCategory.php
@@ -4,10 +4,22 @@ declare(strict_types=1);
 
 namespace Upmind\ProvisionBase\Provider;
 
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\MessageFormatter;
+use GuzzleHttp\Middleware;
+use GuzzleHttp\Psr7\Message;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
 use LogicException;
+use Psr\Http\Message\MessageInterface;
 use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+use RuntimeException;
 use Throwable;
 use Upmind\ProvisionBase\Exception\ProvisionFunctionError;
+use Upmind\ProvisionBase\Provider\Contract\LogsDebugData;
 use Upmind\ProvisionBase\Provider\DataSet\ResultData;
 use Upmind\ProvisionBase\Provider\DataSet\SystemInfo;
 
@@ -29,6 +41,13 @@ abstract class BaseCategory implements Contract\CategoryInterface
      * @var SystemInfo|null
      */
     protected $systemInfo;
+
+    /**
+     * Array containing the history of guzzle requests for this instance.
+     *
+     * @var array<Message[]>
+     */
+    protected $guzzleHistory = [];
 
     /**
      * Returns the result of a successful provision function.
@@ -103,5 +122,74 @@ abstract class BaseCategory implements Contract\CategoryInterface
         }
 
         return $this->systemInfo;
+    }
+
+    /**
+     * Get a guzzle handler stack which logs requests/responses if provider is
+     * an instance of LogsDebugData and $debugLog === true. Requests and responses
+     * will also be stored in $this->guzzleHistory.
+     */
+    protected function getGuzzleHandlerStack(bool $debugLog = false): HandlerStack
+    {
+        $stack = HandlerStack::create();
+
+        $stack->push(Middleware::history($this->guzzleHistory));
+
+        if (!$debugLog || !$this instanceof LogsDebugData) {
+            return $stack;
+        }
+
+        // Rewinds HTTP message body seek position after the stream has been read by Logger middleware
+        $rewindMessageBody = function (MessageInterface $message) {
+            $message->getBody()->rewind();
+            return $message;
+        };
+        // Logs Request/Response HTTP messages
+        $logger = Middleware::log(
+            $this->getLogger(),
+            new MessageFormatter(MessageFormatter::DEBUG . PHP_EOL),
+            LogLevel::DEBUG
+        );
+
+        $stack->push(Middleware::mapRequest($rewindMessageBody), 'Rewind-Request-Stream-After-Logging');
+        $stack->push(Middleware::mapResponse($rewindMessageBody), 'Rewind-Response-Stream-After-Logging');
+        $stack->push($logger, 'Logger');
+
+        return $stack;
+    }
+
+    /**
+     * Returns an assoc array of debug data for the last guzzle request/response
+     * for guzzle clients whose stack was obtained from `$this->getGuzzleHandlerStack()`.
+     *
+     * @return array<array<string[]>>|null
+     */
+    protected function getLastGuzzleRequestDebug(): ?array
+    {
+        /** @var Request|null $lastRequest */
+        $lastRequest = Arr::last($this->guzzleHistory)['request'] ?? null;
+        /** @var Response|null $lastResponse */
+        $lastResponse = Arr::last($this->guzzleHistory)['response'] ?? null;
+
+        if (!$lastRequest) {
+            return null;
+        }
+
+        $debug = [
+            'last_request' => [
+                'method' => $lastRequest->getMethod(),
+                'url' => $lastRequest->getUri()->__toString(),
+            ],
+            'last_response' => null
+        ];
+
+        if ($lastResponse) {
+            $debug['last_response'] = [
+                'http_code' => $lastResponse->getStatusCode(),
+                'body' => Str::limit($lastResponse->getBody()->__toString(), 300),
+            ];
+        }
+
+        return $debug;
     }
 }
